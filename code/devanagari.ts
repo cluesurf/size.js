d2060a7e23825b21ad6af962e5714a60841a7741
Lance Pollard <lp@elk.fm>
2023-06-05 03:12:28 -0700

    more

--- code/devanagari.ts
+++ code/devanagari.ts
@@ -1,8 +1,23 @@
-const NUMBER = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९']
+const list = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९']
+
+const listMake = list.reduce<Record<string, string>>((m, x, i) => {
+  m[i] = x
+  return m
+}, {})
+
+const listRead = list.reduce<Record<string, string>>((m, x, i) => {
+  m[x] = String(i)
+  return m
+}, {})
 
 export function make(n: number) {
   const y = String(n)
     .split('')
-    .map(x => NUMBER[parseInt(x, 10)])
+    .map(x => listMake[x])
+  return y.join('')
+}
+
+export function read(text: string) {
+  const y = text.split('').map(x => listRead[x])
   return y.join('')
 }
